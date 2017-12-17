8ec97dcab7fb48c4b888d85773a6a7cc04a78977
Kliative <klish3@gmail.com>
2017-12-17 10:47:50 +0200

    font-awesome added through webpack

--- skel-boilerplate/webpack.config.js
+++ skel-boilerplate/webpack.config.js
@@ -49,6 +49,14 @@ module.exports = {
                         }
                     }
                 ]
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
             }
         ]
     },
